aece3458a5dd50a47d272e98ca2ab124da4f38ac
Isac Paulsson <isac.97@live.se>
2025-05-13 19:29:30 +0200

    tqdm.

--- src/data_management/make_cv.py
+++ src/data_management/make_cv.py
@@ -1,6 +1,7 @@
 # Mahmut Osmanovic, Isac Paulsson, 2025 April/May
 
 import os
+from tqdm import tqdm
 import numpy as np
 
 def write_to_file(file_list, file_name):
@@ -17,7 +18,7 @@ def make_cv(file_names, folds_dir, k=10):
 
     for i in range(k):
         start_index = images_per_fold * i
-        if i == k:
+        if i == k - 1:
             test_set = file_names[start_index :]
         else:
             test_set = file_names[start_index : start_index + images_per_fold]
@@ -30,7 +31,7 @@ def make_cv(file_names, folds_dir, k=10):
         write_to_file(train_set, os.path.join(fold_dir, "train.dat"))
 
 def make_rcv(file_names, folds_dir, k=10, r=10):
-    for i in range(r):
+    for i in tqdm(range(r)):
         fold_dir_r = os.path.join(folds_dir, f"r{i+1}")
         make_cv(file_names, fold_dir_r, k=k)
 
@@ -42,9 +43,10 @@ if __name__ == '__main__':
     parser.add_argument('folds_dir', type=str, help='Directory to store output folds')
     parser.add_argument('k', type=int, help='Number of folds (K)')
     parser.add_argument('r', type=int, help='Number of repetitions (R)')
+    parser.add_argument('--extension', type=str, help='image extension, default ".tif"', default=".tif")
 
     args = parser.parse_args()
 
-    file_names = [f for f in os.listdir(args.labels_dir) if f.endswith(".tif")]
+    file_names = [f for f in os.listdir(args.labels_dir) if f.endswith(args.extension)]
     make_rcv(file_names, args.folds_dir, k=args.k, r=args.r)
 
